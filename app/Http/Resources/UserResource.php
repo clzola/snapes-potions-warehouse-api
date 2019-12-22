561c33fc1e56b745c6aada3d9384f3731f8a7127
clzola <lazar.radinovic@gmail.com>
2019-12-22 19:57:43 +0100

    Adds UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+/**
+ * Class UserResource
+ * @package App\Http\Resources
+ *
+ * @mixin \App\User
+ */
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'profile_picture' => $this->profile_picture,
+            'created_at' => optional($this->created_at)->format('Y-m-d H:i:s'),
+            'updated_at' => optional($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
