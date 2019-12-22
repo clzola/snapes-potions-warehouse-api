2099696db2f43886276ef409deb6f7ff06c918a3
clzola <lazar.radinovic@gmail.com>
2019-12-23 00:02:16 +0100

    Adds IngredientResource

---
+++ app/Http/Resources/IngredientResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class IngredientResource
+ * @package App\Http\Resources
+ *
+ * @mixin \App\Ingredient
+ */
+class IngredientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'picture' => $this->picture,
+            'amount' => $this->amount,
+            'measurement_unit' => $this->measurement_unit,
+            'created_at' => optional($this->created_at)->format('Y-m-d H:i:s'),
+            'updated_at' => optional($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
