52833ef514ede5165714e0cc9ad2e2a43e72d4fa
clzola <lazar.radinovic@gmail.com>
2019-12-22 19:57:22 +0100

    Writes service for storing profile picture

---
+++ app/Services/StoreProfilePictureService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Constraint;
+use Intervention\Image\Facades\Image;
+
+class StoreProfilePictureService
+{
+    /**
+     * @param \Illuminate\Http\UploadedFile|\Illuminate\Http\UploadedFile[] $uploadedFile
+     * @param null $cropParameters
+     * @return string
+     */
+    public function store($uploadedFile, $cropParameters = null)
+    {
+        $path = $uploadedFile->store('public/users/profile_pictures');
+
+        $image = Image::make(storage_path("app/$path"));
+
+        if(!is_null($cropParameters))
+            $image->crop($cropParameters["width"], $cropParameters["height"], $cropParameters["x"], $cropParameters["y"]);
+
+        $image->fit(512, 512, function (Constraint $constraints) {
+            $constraints->upsize();
+            $constraints->aspectRatio();
+        });
+
+        $image->encode("jpg");
+        $image->save();
+
+        if(!Str::endsWith($path, ["jpg", "jpeg"]))
+            Storage::delete($path);
+
+        return $image->basename;
+    }
+}
