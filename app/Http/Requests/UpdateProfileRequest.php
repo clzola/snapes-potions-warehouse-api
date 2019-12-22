f8c77cbe537c5ec4dae52f4f4a6417dc24b7289e
clzola <lazar.radinovic@gmail.com>
2019-12-22 20:38:54 +0100

    Adds UpdateProfileRequest and validation

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // TODO: Check if is authorized to create user
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "title" => "required|string|max:250",
+            "name" => "required|string|max:250",
+            "email" => "required|string|email|max:250|unique:users,email",
+        ];
+    }
+}
