da32c467ccb26d5788158a88e85921e7ec06c64c
clzola <lazar.radinovic@gmail.com>
2019-12-22 20:00:35 +0100

    Adds UsersController with store method

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Services\StoreProfilePictureService;
+use App\User;
+
+class UsersController extends Controller
+{
+    /**
+     * @param CreateUserRequest $request
+     * @param StoreProfilePictureService $service
+     * @return \Illuminate\Http\JsonResponse|UserResource
+     */
+    public function store(CreateUserRequest $request, StoreProfilePictureService $service)
+    {
+        // Save profile picture if sent
+        $profilePictureFileName = null;
+        if ($request->hasFile('profile_picture')) {
+            $profilePictureFileName = $service->store(
+                $request->file("profile_picture"),
+                $request->get('profile_picture_crop', null)
+            );
+        }
+
+        // Create user
+        $user = new User($request->all());
+        $user->password = \Hash::make($user->password);
+        $user->profile_picture = $profilePictureFileName;
+
+        // Save user
+        $user->save();
+
+        // Respond with user
+        return (new UserResource($user))
+            ->response()
+            ->setStatusCode(201);
+    }
+}
