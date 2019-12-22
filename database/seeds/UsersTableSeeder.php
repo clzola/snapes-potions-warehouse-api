fbbf9865c600a7b71ca28a94991b1763685d4076
clzola <lazar.radinovic@gmail.com>
2019-12-22 16:13:30 +0100

    Adds UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::create([
+            'title' => 'Professor',
+            'name' => 'Severus Snape',
+            'email' => 'snape@slytherin.org',
+            'email_verified_at' => now(),
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
