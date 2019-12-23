4087951179ebae01898ba36450f9bda1f4b83895
clzola <lazar.radinovic@gmail.com>
2019-12-24 00:11:10 +0100

    Adds PotionCategory model

---
+++ app/PotionCategory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\PotionCategory
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PotionCategory whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class PotionCategory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "potion_categories";
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "name",
+        "description",
+    ];
+}
