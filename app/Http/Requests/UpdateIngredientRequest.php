05b59d6e2bb5dfe5ba4e996a7a70470fc8fe919e
clzola <lazar.radinovic@gmail.com>
2019-12-24 17:16:56 +0100

    Removes required validation rule and adds validation rules for ingredient picture

--- app/Http/Requests/UpdateIngredientRequest.php
+++ app/Http/Requests/UpdateIngredientRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateIngredientRequest extends FormRequest
 {
@@ -23,11 +24,25 @@ class UpdateIngredientRequest extends FormRequest
      */
     public function rules()
     {
+        $pictureRules = [
+            'mimes:jpeg,bmp,png',
+        ];
+
+        if(!$this->has('picture_crop')) {
+            $pictureRules[] = Rule::dimensions()->ratio(1.0);
+        }
+
         return [
-            "name" => "required|string|max:250",
-            "description" => "required|string",
-            "amount" => "required|integer|gt:0",
-            "measurement_unit" => "required|string|in:g,ml"
+            "name" => "string|max:250",
+            "description" => "string",
+            "amount" => "integer|gt:0",
+            "measurement_unit" => "string|in:g,ml",
+            "picture" => $pictureRules,
+            "picture_crop" => "array",
+            "picture_crop.width" => "required_with:picture_crop|integer",
+            "picture_crop.height" => "required_with:picture_crop|integer",
+            "picture_crop.x" => "required_with:picture_crop|integer",
+            "picture_crop.y" => "required_with:picture_crop|integer",
         ];
     }
 }
