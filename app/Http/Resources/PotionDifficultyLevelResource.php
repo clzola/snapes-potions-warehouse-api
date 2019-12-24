cb0b7818fdc1e60573fe2cdb8ad74bc79cb72865
clzola <lazar.radinovic@gmail.com>
2019-12-24 13:40:57 +0100

    Adds api resource for PotionDifficultyLevel model

---
+++ app/Http/Resources/PotionDifficultyLevelResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class PotionDifficultyLevelResource
+ * @package App\Http\Resources
+ *
+ * @mixin \App\PotionDifficultyLevel
+ */
+class PotionDifficultyLevelResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+        ];
+    }
+}
