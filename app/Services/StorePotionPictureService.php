770aaf08b561dac33803101a299d74ddd7662ebb
clzola <lazar.radinovic@gmail.com>
2019-12-24 16:31:39 +0100

    Adds service for storing potion pictures

---
+++ app/Services/StorePotionPictureService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use Intervention\Image\Constraint;
+use Intervention\Image\Facades\Image;
+
+class StorePotionPictureService
+{
+    /**
+     * @param \Illuminate\Http\UploadedFile|\Illuminate\Http\UploadedFile[] $uploadedFile
+     * @param null $cropParameters
+     * @return string
+     */
+    public function store($uploadedFile, $cropParameters = null)
+    {
+        $path = $uploadedFile->store('public/potions/pictures');
+
+        $image = Image::make(storage_path("app/$path"));
+
+        if(!is_null($cropParameters))
+            $image->crop($cropParameters["width"], $cropParameters["height"], $cropParameters["x"], $cropParameters["y"]);
+
+        $image->fit(512, 512, function (Constraint $constraints) {
+            $constraints->upsize();
+            $constraints->aspectRatio();
+        });
+
+        $image->save();
+
+        return $path;
+    }
+}
