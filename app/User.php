78d6bb17da3c899d5892b8dcbcce1460eff3462b
clzola <lazar.radinovic@gmail.com>
2020-01-13 23:52:52 +0100

    Adds additional claims to JWT token (name, email)

--- app/User.php
+++ app/User.php
@@ -108,6 +108,9 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
     }
 }
