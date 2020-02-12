3574e1f2a308e73dbb1fc217aec436b35e86e986
clzola <lazar.radinovic@gmail.com>
2020-02-12 14:30:52 +0100

    Removes unused imports

--- app/Http/Controllers/Api/IngredientsController.php
+++ app/Http/Controllers/Api/IngredientsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateIngredientRequest;
-use App\Http\Requests\UpdateIngredientPictureRequest;
 use App\Http\Requests\UpdateIngredientRequest;
 use App\Http\Resources\IngredientResource;
 use App\Ingredient;
